197d48cc33d03c4cfc0222ff1eb98cf6ff572414
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-03-04 01:53:25 -0500

    hmmmm

--- os.js
+++ os.js
@@ -1,9 +1,9 @@
 
 $.getJSON('oblique.json', function(data) { 
     
-  var strategy = data.strategies[Math.floor(Math.random()*data.strategies.length)];
-  console.log(strategy);
-  $("#oblique_strategies p").text(strategy);
+  var randomStrategy = data.strategies[Math.floor(Math.random()*data.strategies.length)];
+  console.log(randomStrategy.strategy);
+//  $("#oblique_strategies p").text(strategy);
     
 });
 
