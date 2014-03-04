f2aea84890363e2d80fee2286ac9df65118d225c
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-03-04 01:49:33 -0500

    hell yes!

--- os.js
+++ os.js
@@ -2,7 +2,7 @@
 $.getJSON('oblique.json', function(data) { 
     
   var strategy = data.strategies[Math.floor(Math.random()*data.strategies.length)];
-  console.log(strategy);
-  
+  $("#oblique_strategies p").text(strategy);
+    
 });
 
