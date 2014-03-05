1d12533b90f59f5048184336c4052a95537cd3c6
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-03-05 11:45:47 -0500

    trial and error

--- os.js
+++ os.js
@@ -4,6 +4,10 @@ $.getJSON('oblique.json', function(data) {
   var randomStrategy = data.strategies[Math.floor(Math.random()*data.strategies.length)];
   console.log(randomStrategy.strategy);
   $("#oblique_strategies p").text(randomStrategy.strategy);
+
+  $.each(data.strategies, function(data) {
+    $('ul#fulllist').append('<li class="col-xs-6 col-sm-4 col-md-3 strategy">' + this["strategy"] + '</li>');
+  });
     
 });
 
